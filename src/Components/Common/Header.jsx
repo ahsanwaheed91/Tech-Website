04b5eb20f3455a5bd1e0bd3b596a37a9cfb6a2fe
Ahsan Waheed <ahsanwaheed884@gmail.com>
2024-06-26 18:50:03 +0500

    update

--- src/Components/Common/Header.jsx
+++ src/Components/Common/Header.jsx
@@ -1,4 +1,4 @@
-import React , { useState } from 'react'
+import React, { useState } from 'react'
 import { Anchor, Button, Drawer } from 'antd';
 
 
@@ -14,90 +14,90 @@ const HeaderApp = () => {
   };
   return (
     <div className="container-fluid">
-        <div className="header">
-       <div className="logo">
-        <i class='fas fa-bolt'></i>
-        <a href="https://github.com/ahsanwaheed91/dashboard">Tech</a>
-       </div>
-       <div className="mobileHidden">
+      <div className="header">
+        <div className="logo">
+          <i class='fas fa-bolt'></i>
+          <a href="https://github.com/ahsanwaheed91/dashboard">Tech</a>
+        </div>
+        <div className="mobileHidden">
 
           <Anchor targetOffset='65'
-        items={[
-          {
-            key: 'part-1',
-            href: '#Hero',
-            title: 'Home',
-          },
-          {
-            key: 'part-2',
-            href: '#About',
-            title: 'About',
-          },
-          {
-            key: 'part-3',
-            href: '#Feature',
-            title: 'Feature',
-          },
-          {
-            key: 'part-4',
-            href: '#Faq',
-            title: 'FAQ',
-          },
-          {
-            key: 'part-5',
-            href: '#Pricing',
-            title: 'Pricing',
-          },
-          {
-            key: 'part-6',
-            href: '#Contact',
-            title: 'Contact',
-          },
-        ]}
-        />
+            items={[
+              {
+                key: 'part-1',
+                href: '#Hero',
+                title: 'Home',
+              },
+              {
+                key: 'part-2',
+                href: '#About',
+                title: 'About',
+              },
+              {
+                key: 'part-3',
+                href: '#Feature',
+                title: 'Feature',
+              },
+              {
+                key: 'part-4',
+                href: '#Faq',
+                title: 'FAQ',
+              },
+              {
+                key: 'part-5',
+                href: '#Pricing',
+                title: 'Pricing',
+              },
+              {
+                key: 'part-6',
+                href: '#Contact',
+                title: 'Contact',
+              },
+            ]}
+          />
         </div>
         <div className="mobileVisible">
-        <Button type="primary" onClick={showDrawer}>
-        <i className="fas fa-bars"></i>
-      </Button>
-      <Drawer  onClose={onClose} open={open}>
-      <Anchor targetOffset='65'
-        items={[
-          {
-            key: 'part-1',
-            href: '#Hero',
-            title: 'Home',
-          },
-          {
-            key: 'part-2',
-            href: '#About',
-            title: 'About',
-          },
-          {
-            key: 'part-3',
-            href: '#Feature',
-            title: 'Feature',
-          },
-          {
-            key: 'part-4',
-            href: '#Faq',
-            title: 'FAQ',
-          },
-          {
-            key: 'part-5',
-            href: '#Pricing',
-            title: 'Pricing',
-          },
-          {
-            key: 'part-6',
-            href: '#Contact',
-            title: 'Contact',
-          },
-        ]}
-        />
-      </Drawer>
+          <Button type="primary" onClick={showDrawer}>
+            <i className="fas fa-bars"></i>
+          </Button>
+          <Drawer onClose={onClose} open={open}>
+            <Anchor targetOffset='65'
+              items={[
+                {
+                  key: 'part-1',
+                  href: '#Hero',
+                  title: 'Home',
+                },
+                {
+                  key: 'part-2',
+                  href: '#About',
+                  title: 'About',
+                },
+                {
+                  key: 'part-3',
+                  href: '#Feature',
+                  title: 'Feature',
+                },
+                {
+                  key: 'part-4',
+                  href: '#Faq',
+                  title: 'FAQ',
+                },
+                {
+                  key: 'part-5',
+                  href: '#Pricing',
+                  title: 'Pricing',
+                },
+                {
+                  key: 'part-6',
+                  href: '#Contact',
+                  title: 'Contact',
+                },
+              ]}
+            />
+          </Drawer>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
